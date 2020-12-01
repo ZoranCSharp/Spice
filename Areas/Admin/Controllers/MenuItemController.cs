2d67e148c9156847e53b67f1a75cee602100dc48
Zoran Kovacevic <zorank.java@gmail.com>
2020-12-01 13:57:22 +0100

    Added MenuITem Model and Index page

---
+++ Areas/Admin/Controllers/MenuItemController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public MenuItemController(ApplicationDbContext db, IWebHostEnvironment hostingEnvironment)
+        {
+            _db = db;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var menuItems = await _db.MenuItem.Include(z=>z.Category).Include(z=>z.SubCategory).ToListAsync();
+
+            return View(menuItems);
+        }
+    }
+}
